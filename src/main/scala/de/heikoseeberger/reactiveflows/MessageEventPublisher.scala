68f51e6e7d8aed48260a0e1e66054fe2f585bf6d
Heiko Seeberger <mail@heikoseeberger.de>
2015-02-16 22:59:56 +0100

    Server-sent events

---
+++ src/main/scala/de/heikoseeberger/reactiveflows/MessageEventPublisher.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015 Heiko Seeberger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.heikoseeberger.reactiveflows
+
+import akka.actor.Props
+import de.heikoseeberger.akkasse.stream.EventPublisher
+import java.time.LocalDateTime
+import scala.concurrent.duration.DurationInt
+
+object MessageEventPublisher {
+
+  /** Factory for [[MessageEventPublisher]] `Props`. */
+  def props(bufferSize: Int) = Props(new MessageEventPublisher(bufferSize))
+}
+
+/** A publisher of [[Flow.MessageEvent]]s. */
+class MessageEventPublisher(bufferSize: Int) extends EventPublisher[Flow.MessageEvent](bufferSize) {
+
+  import context.dispatcher
+
+  context.system.scheduler.schedule(2 seconds, 2 seconds) {
+    self ! Flow.MessageAdded("akka", Flow.Message("Akka and AngularJS are a great combination!", LocalDateTime.now()))
+  }
+
+  override protected def receiveEvent = {
+    case event: Flow.MessageEvent => onEvent(event)
+  }
+}
---
+++ src/test/scala/de/heikoseeberger/reactiveflows/ServerSentEventProtocolSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2015 Heiko Seeberger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.heikoseeberger.reactiveflows
+
+import de.heikoseeberger.akkasse.ServerSentEvent
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import org.scalatest.{ Matchers, WordSpec }
+
+class ServerSentEventProtocolSpec extends WordSpec with Matchers {
+
+  import ServerSentEventProtocol._
+
+  "A MessageEvent" should {
+
+    "be converted to a ServerSentEvent" in {
+      val time = LocalDateTime.from(DateTimeFormatter.ISO_LOCAL_DATE_TIME.parse("2015-01-01T00:00:00"))
+      val serverSentEvent = Flow.MessageAdded("akka", Flow.Message("Akka rocks!", time)): ServerSentEvent
+      val expectedData = """|{
+                            |  "flowName": "akka",
+                            |  "message": {
+                            |    "text": "Akka rocks!",
+                            |    "dateTime": "2015-01-01T00:00:00"
+                            |  }
+                            |}""".stripMargin
+      serverSentEvent.data shouldBe expectedData
+      serverSentEvent.eventType shouldBe Some("added")
+    }
+  }
+}
